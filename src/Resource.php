867258b6142e4a18ca47015de0a1c391eee8be63
Elliot Wright <wright.elliot@gmail.com>
2014-09-10 10:45:58 +0100

    Basic HAL resource structure implemented w/ interfaces.

---
+++ src/Resource.php
@@ -0,0 +1,196 @@
+<?php
+
+/**
+ * This file is part of SeerUK\Hal
+ *
+ * (c) Elliot Wright <elliot@elliotwright.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SeerUK\Hal;
+
+use SeerUK\Hal\LinkInterface;
+use SeerUK\Hal\ResourceInterface;
+
+/**
+ * A HAL Resource
+ *
+ * @author Elliot Wright <elliot@elliotwright.co>
+ */
+class Resource implements ResourceInterface
+{
+    /**
+     * @var array
+     */
+    protected $links;
+
+    /**
+     * @var array
+     */
+    protected $resources;
+
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->links     = [];
+        $this->resources = [];
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addLink($name, LinkInterface $link, $append = null)
+    {
+        if ($append) {
+            $appended = [];
+
+            if ($this->hasLink($name)) {
+                $temp = $this->getLink($name);
+
+                if (is_array($temp)) {
+                    $appended = $temp;
+                } else {
+                    $appended[] = $temp;
+                }
+            }
+
+            $appended[] = $link;
+
+            $this->links[$name] = $appended;
+
+            return $this;
+        }
+
+        $this->links[$name] = $link;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addLinks(array $links)
+    {
+        foreach ($links as $name => $link) {
+            $this->addLink($name, $link);
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getLink($name)
+    {
+        return $this->links[$name];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getLinks()
+    {
+        return $this->links;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function hasLink($name)
+    {
+        return isset($this->links[$name]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function removeLink($name)
+    {
+        unset($this->links[$name]);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addResource($name, ResourceInterface $resource, $append = null)
+    {
+        if ($append) {
+            $appended = [];
+
+            if ($this->hasResource($name)) {
+                $temp = $this->getResource($name);
+
+                if (is_array($temp)) {
+                    $appended = $temp;
+                } else {
+                    $appended[] = $temp;
+                }
+            }
+
+            $appended[] = $resource;
+
+            $this->resources[$name] = $appended;
+
+            return $this;
+        }
+
+        $this->resources[$name] = $resource;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addResources(array $resources)
+    {
+        foreach ($resources as $name => $resource) {
+            $this->addResource($name, $resource);
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getResource($name)
+    {
+        return $this->resources[$name];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getResources()
+    {
+        return $this->resources;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function hasResource($name)
+    {
+        return isset($this->resources[$name]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function removeResource($name)
+    {
+        unset($this->resources[$name]);
+
+        return $this;
+    }
+}
---
+++ src/ResourceInterface.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * This file is part of SeerUK\Hal
+ *
+ * (c) Elliot Wright <elliot@elliotwright.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SeerUK\Hal;
+
+use SeerUK\Hal\LinkInterface;
+use SeerUK\Hal\ResourceInterface;
+
+/**
+ * A HAL Resource Interface
+ *
+ * @author Elliot Wright <elliot@elliotwright.co>
+ */
+interface ResourceInterface
+{
+    /**
+     * Add link
+     *
+     * @param string        $name
+     * @param LinkInterface $link
+     * @param boolean       $append
+     *
+     * @return ResourceInterface
+     */
+    public function addLink($name, LinkInterface $link, $append = null);
+
+    /**
+     * Add links
+     *
+     * @param array $links
+     *
+     * @return ResourceInterface
+     */
+    public function addLinks(array $links);
+
+    /**
+     * Get link
+     *
+     * @param string $name
+     *
+     * @return LinkInterface
+     */
+    public function getLink($name);
+
+    /**
+     * Get links
+     *
+     * @return array
+     */
+    public function getLinks();
+
+    /**
+     * Has link?
+     *
+     * @param string  $name
+     *
+     * @return boolean
+     */
+    public function hasLink($name);
+
+    /**
+     * Remove link
+     *
+     * @param string $name
+     *
+     * @return ResourceInterface
+     */
+    public function removeLink($name);
+
+    /**
+     * Add resource
+     *
+     * @param string            $name
+     * @param ResourceInterface $resource
+     * @param boolean           $append
+     *
+     * @return ResourceInterface
+     */
+    public function addResource($name, ResourceInterface $resource, $append = null);
+
+    /**
+     * Add resources
+     *
+     * @param array $resources
+     *
+     * @return ResourceInterface
+     */
+    public function addResources(array $resources);
+
+    /**
+     * Get resource
+     *
+     * @param string $name
+     *
+     * @return ResourceInterface
+     */
+    public function getResource($name);
+
+    /**
+     * Get resources
+     *
+     * @return array
+     */
+    public function getResources();
+
+    /**
+     * Has resource?
+     *
+     * @param string  $name
+     *
+     * @return boolean
+     */
+    public function hasResource($name);
+
+    /**
+     * Remove resource
+     *
+     * @param  string $name
+     *
+     * @return ResourceInterface
+     */
+    public function removeResource($name);
+}
